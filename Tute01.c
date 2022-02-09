e354adf8be0d4a7a532aa58fc1a87f20585f4f04
IT21251900 <99325216+sanjanaDinith@users.noreply.github.com>
2022-02-09 15:45:45 +0530

    Update Tute01.c
    input marks of two subjects and print average

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,16 @@
 #include <stdio.h>
 
 int main() {
-  
+  int mark, i, sum = 0;
+  float avg;
+  for(i=0; i<2; i++){
+   printf("Enter mark %d :",i);
+   scanf("%d", &mark);
+   sum = sum + mark;
+  }
+   
+  avg = sum / i - 1;
+   printf("avg id %.2f", avg);
   return 0;
 }
 
