71e1c996f02be719ae8e2a28a90ac4057ec237bd
IT21251900 <99325216+sanjanaDinith@users.noreply.github.com>
2022-02-09 16:21:14 +0530

    Update Tute02.c
    program to calculate the amount to be paid for a rented vehicle.
    
    •	Input the distance the van has travelled
    •	The first 30 km is at a rate of 50/= per km.
    •	The remaining distance is calculated at the rate of 40/= per km.

--- Tute02.c
+++ Tute02.c
@@ -19,6 +19,17 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 #include <stdio.h>
 
 int main() {
-  
+  int distance;
+  float price;
+  printf("Enter the distance : ");
+  scanf("%d",&distance);
+  if(distance <= 30){
+    printf("%.2f",distance * 50); 
+  }
+  else{
+  printf("%.2f", (30 * 50) + ((distance - 30) * 40 ); 
+  }
+         
+    
   return 0;
 }
