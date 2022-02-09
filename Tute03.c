dada7b039f5c9e2ab6b2f4981c0ce1087c8860bd
IT21251900 <99325216+sanjanaDinith@users.noreply.github.com>
2022-02-09 16:42:39 +0530

    Update Tute03.c
    Calculate the sum of the numbers from 1 to n.

--- Tute03.c
+++ Tute03.c
@@ -13,7 +13,17 @@ sum = 1+2+3+...+10 = 55 */
 
 #include <stdio.h>
 int main() {
+  int i, n; 
+  int sum = 0;
+  printf("ENTER NUMBER");
+  scanf("%d", &n);
   
+  
+  for(i = 0; i < n; i++){
+   sum = sum + i;
+  }
+  
+  printf("%d", sum);
   return 0;
 }
 
